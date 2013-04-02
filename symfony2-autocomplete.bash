9ed0bcf8f7e734beb42c162ac72088a8fc70e275
Diego Barrio Hortigüela <diegobarrioh@diegobarrioh.local>
2013-04-02 12:01:41 +0200

    added autocompletion for alias sf2

--- symfony2-autocomplete.bash
+++ symfony2-autocomplete.bash
@@ -51,10 +51,49 @@ HEREDOC
     return 0;
 }
 
+# Author.: Ole J
+# Date...: 23.03.2008
+# License: Whatever
+
+# Wraps a completion function
+# make-completion-wrapper <actual completion function> <name of new func.>
+#                         <command name> <list supplied arguments>
+# eg.
+#   alias agi='apt-get install'
+#   make-completion-wrapper _apt_get _apt_get_install apt-get install
+# defines a function called _apt_get_install (that's $2) that will complete
+# the 'agi' alias. (complete -F _apt_get_install agi)
+#
+function make-completion-wrapper () {
+    local function_name="$2"
+    local arg_count=$(($#-3))
+    local comp_function_name="$1"
+    shift 2
+    local function="
+function $function_name {
+    ((COMP_CWORD+=$arg_count))
+    COMP_WORDS=( "$@" \${COMP_WORDS[@]:1} )
+    "$comp_function_name"
+    return 0
+}"
+    eval "$function"
+}
+
+# and now the commands that are specific to this SO question
+
+alias sf2='./app/console'
+
+# we create a _console_mine function that will do the completion for "sf"
+# using the completion function "_console"
+make-completion-wrapper _console _console_mine ./app/console
+
 complete -F _console console
 complete -F _console console-dev
 complete -F _console console-test
 complete -F _console console-prod
 complete -F _console console-staging
 complete -F _console Symfony
+# we tell bash to actually use _console_mine to complete "sf2"
+complete -o bashdefault -o default -o nospace -F _console_mine sf2
+
 COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
